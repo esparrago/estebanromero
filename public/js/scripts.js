a4a9bf14bc3566d51169711ca1c1b820aa950fa1
esparrago <manoloromero13@hotmail.com>
2014-10-01 18:33:45 -0500

    responsive issues1

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -16,6 +16,42 @@ $(function(){
         loopCount: false, 
       });
   });
+  
+$(document).ready(function() {
+  
+  var element =  $( "#chart" ).height();
+  var view = $( ".sections" ).height();
+
+  if (element > view) {
+
+    $( ".sections" ).css( "height", "auto" );
+
+  }
+
+
+});
+
+
+  $(function(  ){
+    $.fn.centerizer = function() {
+      var altocontainer = $( ".sections" ).height();
+      console.log(altocontainer);
+      var altoelement =  this.height();
+      console.log(altoelement);
+      var subcenter = (altocontainer - altoelement);
+      console.log(subcenter);
+      var center = (subcenter / 2);
+      var titles = $( ".titles" ).height();
+      var centertotal = (center - titles);
+      console.log(centertotal);
+
+      $( this ).css( "margin-top", centertotal );
+  };
+});
+
+$(document).ready(function() {
+$("#textabout p").centerizer();
+});
 
 
 $( "#menuicon" ).click(function() {
@@ -75,11 +111,27 @@ $(document).ready(function() {
   });
 });
 
+
+
+
+  $(window).on("swipe",function(){setTimeout(
+      function(){
+        var _top = $(window).scrollTop();
+        var individualDivHeight = $( window ).height();
+        var _cur_top = $(window).scrollTop();
+        var totalHeight = $('#container').height();
+        var posToScroll = Math.round(_cur_top / individualDivHeight) * individualDivHeight;
+        $('html, body').stop().animate({scrollTop: posToScroll}, 500);
+        console.log("swipe")
+        }, 800);
+  });
+                 
+ 
 $(".fixedarrow" ).click (function(){
     var nextdiv = $( window ).height();
     $('html, body').animate({
-        scrollTop: ($(window).scrollTop() + nextdiv)
-    });
+        scrollTop: ($(window).scrollTop() + nextdiv)}
+        , 1000);
        
 });
 
@@ -129,83 +181,3 @@ $(function (  ) {
                      });
 });
 
-
-function cargaSendMail(){
- 
- 
-    $("#c_enviar").attr("disabled", true);
- 
-    $(".c_error").remove();
- 
-    var filter=/^[A-Za-z][A-Za-z0-9_]*@[A-Za-z0-9_]+.[A-Za-z0-9_.]+[A-za-z]$/;
-    var s_email = $('#c_mail').val();
-    var s_name = $('#c_name').val();    
-    var s_msg = $('#c_msg').val();
- 
-    if (filter.test(s_email)){
-    sendMail = "true";
-    } else{
-    $('#c_mail').after("<span class='c_error' id='c_error_mail'>Ingrese e-mail valido.</span>");
-     //aplicamos color de borde si el se encontro algun error en el envio
-    $('#contactform').css("border-color","#e74c3c");   
-    sendMail = "false";
-    }
-    if (s_name.length == 0 ){
-    $('#c_name').after( "<span class='c_error' id='c_error_name'>Ingrese su nombre.</span>" );
-    var sendMail = "false";
-    }
-    if (s_msg.length == 0 ){
-    $('#c_msg').after( "<span class='c_error' id='c_error_msg'>Ingrese mensaje.</span>" );
-    var sendMail = "false";
-    }
- 
-    
-    if(sendMail == "true"){
-     
-     var datos = {
- 
-             "nombre" : $('#c_name').val(),
- 
-             "email" : $('#c_mail').val(),
- 
-             "mensaje" : $('#c_msg').val()
- 
-     };
- 
-     $.ajax({
- 
-             data:  datos,
-             // hacemos referencia al archivo contacto.php
-             url:   'contacto.php',
- 
-             type:  'post',
- 
-             beforeSend: function () {
-             //aplicamos color de borde si el envio es exitoso
-                    $('#contactform').css("border-color","#25A25A");
- 
-                     $("#c_enviar").val("Enviando...");
- 
-             },
- 
-             success:  function (response) {
- 
-                    $('form')[0].reset(); 
-                    $("#c_enviar").val("Enviar");
-                    $("#c_information p").html(response);
-                    $("#c_information").fadeIn('slow');
-                    $("#c_enviar").removeAttr("disabled");
-                    alert(enviado);
- 
- 
-             }
- 
-     });
- 
-} else{
-    $("#c_enviar").removeAttr("disabled");
-}
- 
-}
-
-    
